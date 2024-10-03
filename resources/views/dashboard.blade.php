da6208dee4179f9e2094d7bf7de77121ab91fb67
Hugo Sauvage <hugosauvage@MacBook-Air-de-Hugo.local>
2024-10-03 20:39:48 +0200

    tailwind removed

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div>
+            <div>
+                <div>
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
